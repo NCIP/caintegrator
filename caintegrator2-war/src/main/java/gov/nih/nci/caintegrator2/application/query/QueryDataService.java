77ac5481ef2ce9b81ee85c8e507bf77e14e464e1
Eric Tavela <eric.tavela2@nih.gov>
2008-05-22 13:16:56 +0000

    Moved QueryResult class; added serialVersionUIDs. Peer reviewed by Michael Holck.

--- caintegrator2-war/src/main/java/gov/nih/nci/caintegrator2/application/query/QueryDataService.java
+++ caintegrator2-war/src/main/java/gov/nih/nci/caintegrator2/application/query/QueryDataService.java
@@ -87,7 +87,6 @@
 package gov.nih.nci.caintegrator2.application.query;
 
 import gov.nih.nci.caintegrator2.domain.application.Query;
-import gov.nih.nci.caintegrator2.domain.application.QueryResult;
 
 /**
  * Interface to the service which handles query data.
