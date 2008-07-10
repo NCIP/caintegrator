c8686fd238d43769f80c7603750893804340e1b5
Unknown <gursesl@mail.nih.gov>
2008-07-10 06:44:50 +0000

    Fit acceptance test targets and samples tests

---
+++ software/caintegrator2-war/test/acceptance/src/java/gov/nih/nci/caintegrator2/Operation.java
@@ -0,0 +1,10 @@
+package gov.nih.nci.caintegrator2;
+
+public class Operation {
+
+	public int index; // location within the history
+
+	public double left;
+	public double right;
+	public char operator;
+}
