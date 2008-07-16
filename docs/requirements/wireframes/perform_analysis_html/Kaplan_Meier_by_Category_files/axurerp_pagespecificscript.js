ebe9f78e02a408e089e6634b6981478891971f49
Will Fitzhugh <wfitzhugh@5amsolutions.com>
2008-07-16 20:52:38 +0000

    updates, better on-page doc

--- docs/requirements/wireframes/perform_analysis_html/Kaplan_Meier_by_Category_files/axurerp_pagespecificscript.js
+++ docs/requirements/wireframes/perform_analysis_html/Kaplan_Meier_by_Category_files/axurerp_pagespecificscript.js
@@ -45,28 +45,36 @@ function OnLoad() {
 var u16 = document.getElementById('u16');
 gv_vAlignTable['u16'] = 'top';
 var u7 = document.getElementById('u7');
-gv_vAlignTable['u7'] = 'top';
+gv_vAlignTable['u7'] = 'center';
 var u15 = document.getElementById('u15');
 
 var u2 = document.getElementById('u2');
-gv_vAlignTable['u2'] = 'top';
-var u19 = document.getElementById('u19');
-gv_vAlignTable['u19'] = 'top';
-var u13 = document.getElementById('u13');
 
+var u19 = document.getElementById('u19');
+
+var u13 = document.getElementById('u13');
+gv_vAlignTable['u13'] = 'top';
+var u22 = document.getElementById('u22');
+gv_vAlignTable['u22'] = 'top';
 var u12 = document.getElementById('u12');
 
 var u5 = document.getElementById('u5');
-gv_vAlignTable['u5'] = 'top';
+gv_vAlignTable['u5'] = 'center';
 var u8 = document.getElementById('u8');
 gv_vAlignTable['u8'] = 'top';
 var u10 = document.getElementById('u10');
 gv_vAlignTable['u10'] = 'top';
 var u0 = document.getElementById('u0');
 
+var u25 = document.getElementById('u25');
+gv_vAlignTable['u25'] = 'top';
+var u21 = document.getElementById('u21');
+
 var u17 = document.getElementById('u17');
 
 var u3 = document.getElementById('u3');
+gv_vAlignTable['u3'] = 'center';
+var u23 = document.getElementById('u23');
 
 var u14 = document.getElementById('u14');
 gv_vAlignTable['u14'] = 'top';
@@ -74,12 +82,16 @@ var u6 = document.getElementById('u6');
 
 var u9 = document.getElementById('u9');
 
+var u20 = document.getElementById('u20');
+gv_vAlignTable['u20'] = 'top';
 var u1 = document.getElementById('u1');
 gv_vAlignTable['u1'] = 'center';
 var u11 = document.getElementById('u11');
-
+gv_vAlignTable['u11'] = 'top';
 var u18 = document.getElementById('u18');
-gv_vAlignTable['u18'] = 'top';
+
+var u24 = document.getElementById('u24');
+gv_vAlignTable['u24'] = 'top';
 var u4 = document.getElementById('u4');
-gv_vAlignTable['u4'] = 'top';
+
 if (window.OnLoad) OnLoad();
--- docs/requirements/wireframes/perform_analysis_html/Kaplan_Meier_by_Gene_Expression_files/axurerp_pagespecificscript.js
+++ docs/requirements/wireframes/perform_analysis_html/Kaplan_Meier_by_Gene_Expression_files/axurerp_pagespecificscript.js
@@ -43,37 +43,69 @@ function OnLoad() {
 }
 
 var u16 = document.getElementById('u16');
-gv_vAlignTable['u16'] = 'top';
+
 var u7 = document.getElementById('u7');
-gv_vAlignTable['u7'] = 'top';
+gv_vAlignTable['u7'] = 'center';
+var u28 = document.getElementById('u28');
+gv_vAlignTable['u28'] = 'top';
+var u30 = document.getElementById('u30');
+
 var u15 = document.getElementById('u15');
-gv_vAlignTable['u15'] = 'top';
+
 var u2 = document.getElementById('u2');
-gv_vAlignTable['u2'] = 'top';
+
+var u19 = document.getElementById('u19');
+gv_vAlignTable['u19'] = 'top';
 var u13 = document.getElementById('u13');
+gv_vAlignTable['u13'] = 'top';
+var u31 = document.getElementById('u31');
+gv_vAlignTable['u31'] = 'top';
+var u22 = document.getElementById('u22');
 
 var u12 = document.getElementById('u12');
-
+gv_vAlignTable['u12'] = 'top';
 var u5 = document.getElementById('u5');
-
+gv_vAlignTable['u5'] = 'center';
 var u8 = document.getElementById('u8');
 
 var u10 = document.getElementById('u10');
 
+var u32 = document.getElementById('u32');
+gv_vAlignTable['u32'] = 'top';
 var u0 = document.getElementById('u0');
 
+var u26 = document.getElementById('u26');
+gv_vAlignTable['u26'] = 'top';
+var u25 = document.getElementById('u25');
+gv_vAlignTable['u25'] = 'top';
+var u21 = document.getElementById('u21');
+gv_vAlignTable['u21'] = 'top';
+var u17 = document.getElementById('u17');
+gv_vAlignTable['u17'] = 'top';
 var u3 = document.getElementById('u3');
-gv_vAlignTable['u3'] = 'top';
+gv_vAlignTable['u3'] = 'center';
+var u29 = document.getElementById('u29');
+gv_vAlignTable['u29'] = 'top';
+var u23 = document.getElementById('u23');
+
 var u14 = document.getElementById('u14');
-gv_vAlignTable['u14'] = 'top';
+
 var u6 = document.getElementById('u6');
 
 var u9 = document.getElementById('u9');
-gv_vAlignTable['u9'] = 'top';
+gv_vAlignTable['u9'] = 'center';
+var u20 = document.getElementById('u20');
+
 var u1 = document.getElementById('u1');
 gv_vAlignTable['u1'] = 'center';
 var u11 = document.getElementById('u11');
-gv_vAlignTable['u11'] = 'top';
+gv_vAlignTable['u11'] = 'center';
+var u18 = document.getElementById('u18');
+
+var u24 = document.getElementById('u24');
+gv_vAlignTable['u24'] = 'top';
 var u4 = document.getElementById('u4');
+
+var u27 = document.getElementById('u27');
 
 if (window.OnLoad) OnLoad();
