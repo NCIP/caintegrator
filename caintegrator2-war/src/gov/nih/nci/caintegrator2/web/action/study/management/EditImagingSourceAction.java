9730d55177193f033047e5b5347addf140a8ae7c
Lakshmanan Rajagopalan <lakshmanan.rajagopalan@nih.gov>
2008-08-18 15:33:12 +0000

    Created for Imaging

---
+++ caintegrator2-war/src/gov/nih/nci/caintegrator2/web/action/study/management/EditImagingSourceAction.java
@@ -0,0 +1,14 @@
+package gov.nih.nci.caintegrator2.web.action.study.management;
+
+public class EditImagingSourceAction extends AbstractGenomicSourceAction{
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String execute() {
+        return SUCCESS;
+    }
+
+}
