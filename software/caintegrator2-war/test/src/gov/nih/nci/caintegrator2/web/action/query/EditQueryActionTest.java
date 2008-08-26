cc6760e7cb8ec866bd537260c2b828ab10bd3bee
Timothy Andrews <tandrews@scenpro.com>
2008-08-26 16:14:32 +0000

    Removing unused imports.

--- software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/web/action/query/EditQueryActionTest.java
+++ software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/web/action/query/EditQueryActionTest.java
@@ -86,9 +86,6 @@
 package gov.nih.nci.caintegrator2.web.action.query;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
