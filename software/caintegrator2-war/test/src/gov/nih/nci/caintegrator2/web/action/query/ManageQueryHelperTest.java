44df857ed309a936461b2fb171b77e9b8796ce48
Mark Lewis <mark.lewis@nih.gov>
2008-09-14 23:05:47 +0000

    Removed Set import.

--- software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/web/action/query/ManageQueryHelperTest.java
+++ software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/web/action/query/ManageQueryHelperTest.java
@@ -89,7 +89,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
