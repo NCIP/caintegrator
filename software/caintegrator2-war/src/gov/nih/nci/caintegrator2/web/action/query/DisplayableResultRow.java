d8a5cad9cfc2042a347a0f43d5235a97fa420ccc
Lakshmanan Rajagopalan <lakshmanan.rajagopalan@nih.gov>
2008-10-06 20:32:35 +0000

    Made Changes for the NCIA Link So that source will not be null anymore and View Image is working fine Code Reviewed By Eric GForge defect # 16800

--- software/caintegrator2-war/src/gov/nih/nci/caintegrator2/web/action/query/DisplayableResultRow.java
+++ software/caintegrator2-war/src/gov/nih/nci/caintegrator2/web/action/query/DisplayableResultRow.java
@@ -176,9 +176,8 @@ public class DisplayableResultRow {
     public String getNciaLink() {
         if (getImageSeries() != null) {
             StringBuffer stringBuffer = new StringBuffer();
-            stringBuffer.append("https://imaging-dev.nci.nih.gov/ncia/faces/referencedImages.tiles?source=");
-            stringBuffer.append(getImageSeries().getImageStudy().getNciaTrialIdentifier());
-            stringBuffer.append("&image1TrialId=");
+            stringBuffer.append("https://imaging-dev.nci.nih.gov/ncia/faces/" 
+            + "referencedImages.tiles?source=ISPY&image1TrialId=");
             stringBuffer.append(getImageSeries().getImageStudy().getNciaTrialIdentifier());
             stringBuffer.append("&image1PatientId=");
             stringBuffer.append(getImageSeries().getImageStudy().getAssignment().getIdentifier());
