7788aa1582aa0562bbeb347de5025cb6a95fe8d0
Nghiem Nguyen <nguyenn@mail.nih.gov>
2008-11-20 20:31:05 +0000

    Item 17589 - Cannot enter permissible values for non-caDSR datatypes with more test, reviewed by Eric

--- caintegrator2-war/test/src/gov/nih/nci/caintegrator2/web/action/study/management/DefineFileColumnActionTest.java
+++ caintegrator2-war/test/src/gov/nih/nci/caintegrator2/web/action/study/management/DefineFileColumnActionTest.java
@@ -3,18 +3,23 @@ package gov.nih.nci.caintegrator2.web.action.study.management;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import gov.nih.nci.caintegrator2.AcegiAuthenticationStub;
 import gov.nih.nci.caintegrator2.application.study.AnnotationFieldDescriptor;
 import gov.nih.nci.caintegrator2.application.study.AnnotationFile;
 import gov.nih.nci.caintegrator2.application.study.FileColumn;
 import gov.nih.nci.caintegrator2.application.study.StudyManagementServiceStub;
 import gov.nih.nci.caintegrator2.domain.annotation.AnnotationDefinition;
 
+import java.util.HashMap;
+
+import org.acegisecurity.context.SecurityContextHolder;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
 
 
 public class DefineFileColumnActionTest {
@@ -24,7 +29,11 @@ public class DefineFileColumnActionTest {
 
     @Before
     public void setUp() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("study-management-action-test-config.xml", DefineFileColumnActionTest.class); 
+        ApplicationContext context = new ClassPathXmlApplicationContext("study-management-action-test-config.xml", DefineFileColumnActionTest.class);
+
+        SecurityContextHolder.getContext().setAuthentication(new AcegiAuthenticationStub());
+        ActionContext.getContext().setSession(new HashMap<String, Object>());
+
         action = (DefineFileColumnAction) context.getBean("defineFileColumnAction");
         studyManagementServiceStub = (StudyManagementServiceStub) context.getBean("studyManagementService");
         studyManagementServiceStub.clear();
@@ -115,6 +124,36 @@ public class DefineFileColumnActionTest {
         assertEquals(Action.SUCCESS, action.createNewDefinition());
         assertTrue(studyManagementServiceStub.createDefinitionCalled);
         assertFalse(action.isReadOnly());
+        
+    }
+    
+    @Test
+    public void testIsColumnTypeAnnotation() {
+        action.setColumnType("Annotation");
+        assertTrue(action.isColumnTypeAnnotation());
+        action.setColumnType("Identifier");
+        assertFalse(action.isColumnTypeAnnotation());
+    }
+    
+    @Test
+    public void testIsPermissibleOn() {
+        action.setColumnType("Identifier");
+        assertFalse(action.isPermissibleOn());
+        action.setColumnType("Annotation");
+        action.setFileColumn(new FileColumn());
+        action.getFileColumn().setFieldDescriptor(new AnnotationFieldDescriptor());
+        assertFalse(action.isPermissibleOn());
+        action.getFileColumn().getFieldDescriptor().setDefinition(new AnnotationDefinition());
+        assertTrue(action.isPermissibleOn());
+        
+    }
+    
+    @Test
+    public void testUpdateFileColumn() {
+        action.setColumnType("Annotation");
+        action.setFileColumn(new FileColumn());
+        action.getFileColumn().setFieldDescriptor(new AnnotationFieldDescriptor());
+        assertEquals(Action.SUCCESS, action.updateFileColumn());
         
     }
 }
