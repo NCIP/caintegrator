09374b8abf7f1f2407e34f6ac873ce2283756527
Eric Tavela <eric.tavela2@nih.gov>
2008-12-22 18:37:08 +0000

    Added design loading for HG-U133A. Peer reviewed by JP.

---
+++ software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/application/arraydata/LoadAffymetrixDesignsTestIntegration.java
@@ -0,0 +1,63 @@
+package gov.nih.nci.caintegrator2.application.arraydata;
+
+import gov.nih.nci.caintegrator2.TestArrayDesignFiles;
+import gov.nih.nci.caintegrator2.data.CaIntegrator2Dao;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.springframework.test.AbstractTransactionalSpringContextTests;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class LoadAffymetrixDesignsTestIntegration extends AbstractTransactionalSpringContextTests {
+    
+    private ArrayDataService arrayDataService;
+    private CaIntegrator2Dao dao;
+    
+    public LoadAffymetrixDesignsTestIntegration() {
+        setDefaultRollback(false);
+    }
+    
+    protected String[] getConfigLocations() {
+        return new String[] {"classpath*:/**/service-test-integration-config.xml"};
+    }
+
+    @Test
+    public void testLoadArrayDesign() throws PlatformLoadingException, AffymetrixCdfReadException {
+        checkLoadArrayDesign(TestArrayDesignFiles.HG_U133A_CDF_FILE, TestArrayDesignFiles.HG_U133A_ANNOTATION_FILE);
+    }
+
+    private void checkLoadArrayDesign(File cdfFile, File annotationFile) throws PlatformLoadingException, AffymetrixCdfReadException {
+        ArrayDesignChecker.checkLoadArrayDesign(cdfFile, annotationFile, arrayDataService);
+    }
+
+    /**
+     * @return the arrayDataService
+     */
+    public ArrayDataService getArrayDataService() {
+        return arrayDataService;
+    }
+
+    /**
+     * @param arrayDataService the arrayDataService to set
+     */
+    public void setArrayDataService(ArrayDataService arrayDataService) {
+        this.arrayDataService = arrayDataService;
+    }
+
+    /**
+     * @return the dao
+     */
+    public CaIntegrator2Dao getDao() {
+        return dao;
+    }
+
+    /**
+     * @param dao the dao to set
+     */
+    public void setDao(CaIntegrator2Dao dao) {
+        this.dao = dao;
+    }
+
+}
