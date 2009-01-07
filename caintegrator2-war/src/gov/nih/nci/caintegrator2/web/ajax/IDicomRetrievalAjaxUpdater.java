1d47f895aeedf6917c26c6aed967a6318d55f743
Timothy Andrews <tandrews@scenpro.com>
2009-01-07 20:01:26 +0000

    Fixed a DWR issue that was making DicomRetrievalAjaxUpdater a singleton bean instead of prototype (thus making one Updater for multiple users and causing erroneous behavior on the JSP when simultaneously trying to do dicom retrieval)
    GForge Task #18551
    Peer reviewed by Eric.

--- caintegrator2-war/src/gov/nih/nci/caintegrator2/web/ajax/IDicomRetrievalAjaxUpdater.java
+++ caintegrator2-war/src/gov/nih/nci/caintegrator2/web/ajax/IDicomRetrievalAjaxUpdater.java
@@ -85,114 +85,15 @@
  */
 package gov.nih.nci.caintegrator2.web.ajax;
 
-import gov.nih.nci.caintegrator2.external.ncia.NCIADicomJob;
-import gov.nih.nci.caintegrator2.external.ncia.NCIAFacade;
-import gov.nih.nci.caintegrator2.web.DisplayableUserWorkspace;
-
-import org.directwebremoting.WebContext;
-import org.directwebremoting.WebContextFactory;
-import org.directwebremoting.proxy.dwr.Util;
-
 /**
- * This is an object which is turned into an AJAX javascript file using the DWR framework.  The whole purpose
- * is to update the jsp page with information based on the DicomRetrievalAjaxRunner's status updates.  It
- * uses the reverse-ajax technology of DWR to achieve this.
+ * This interface is to allow DWR to javascript remote the methods using Spring. 
  */
-public class DicomRetrievalAjaxUpdater implements IDicomRetrievalAjaxUpdater { 
-    private Util utilThis;
-    private NCIAFacade nciaFacade;
-    private NCIADicomJob dicomJob;
-    
-
-    /**
-     * {@inheritDoc}
-     */
-    public void runDicomJob() {
-        WebContext wctx = WebContextFactory.get();
-        DisplayableUserWorkspace workspace = (DisplayableUserWorkspace) 
-            wctx.getSession().getAttribute("displayableWorkspace");
-        utilThis = new Util(wctx.getScriptSession());
-        dicomJob = workspace.getDicomJob();
-        
-        if (dicomJob != null) {
-            initializeJobDescription();
-            DicomRetrievalAjaxRunner runner = new DicomRetrievalAjaxRunner(this);
-            new Thread(runner).start();
-        } else {
-            addErrorMessage("No Dicom Job was found on the Session");
-        }
-    }
-    
-    private void initializeJobDescription() {
-        if (!dicomJob.getImageSeriesIDs().isEmpty()) {
-            utilThis.setValue("imageSeriesStatus", "<h3>Image Series UIds</h3>", false);
-            utilThis.addOptions("imageSeriesList", dicomJob.getImageSeriesIDs().
-                                                   toArray(new String[dicomJob.getImageSeriesIDs().size()]));
-        }
-        
-        if (!dicomJob.getImageStudyIDs().isEmpty()) {
-            utilThis.setValue("imageStudyStatus", "<h3>Image Study UIds</h3>");
-            utilThis.addOptions("imageStudyList", dicomJob.getImageStudyIDs().
-                                                   toArray(new String[dicomJob.getImageStudyIDs().size()]));
-        }
-    }
+public interface IDicomRetrievalAjaxUpdater {
     
     /**
-     * 
-     * @param currentStatus sets status on JSP page.
+     * Retrieves the DicomJob from the session and creates a new DicomRetrievalAjaxRunner thread and starts it.
+     * This function is called directly from the dicomJobAjax_tile.jsp page using DWR's reverse ajax mechanism.
      */
-    public void updateCurrentStatus(String currentStatus) {
-        utilThis.setValue("currentStatus", currentStatus);
-    }
-    
-    /**
-     * The final update of the dynamic test, assuming a successful finish occurs.
-     */
-    public void finish() {
-        completeJob();
-        
-        String finalizedText = "<b> Job Finished </b> <br>" 
-                               + "<a href=\"downloadDicomFile.action\">Download DICOM Files</a>";
-        utilThis.setValue("finalizedText", finalizedText, false);
-    }
-    
-    /**
-     * Dynamically adds an error message to the page.
-     * @param message error message to add.
-     */
-    public void addErrorMessage(String message) {
-        completeJob();
-        utilThis.setValue("errorMessages", message);
-    }
-
-    private void completeJob() {
-        dicomJob.setCurrentlyRunning(false);
-        removeStatusDiv();
-    }
-    
-    private void removeStatusDiv() {
-        utilThis.setValue("overallStatusDiv", "");
-    }
-
-    /**
-     * @return the nciaFacade
-     */
-    public NCIAFacade getNciaFacade() {
-        return nciaFacade;
-    }
-
-    /**
-     * @param nciaFacade the nciaFacade to set
-     */
-    public void setNciaFacade(NCIAFacade nciaFacade) {
-        this.nciaFacade = nciaFacade;
-    }
-
-    /**
-     * @return the dicomJob
-     */
-    public NCIADicomJob getDicomJob() {
-        return dicomJob;
-    }
+    void runDicomJob();
 
 }
