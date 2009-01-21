76691b9f84d7da4e32f81e09d103903fcaf7bb14
Nghiem Nguyen <nguyenn@mail.nih.gov>
2009-01-21 18:16:50 +0000

    Item 18472 - USBA to delete study - Bug fix when deleting a default study subscription (Roll back), reviewed by Eric

--- software/caintegrator2-war/src/gov/nih/nci/caintegrator2/application/workspace/WorkspaceServiceImpl.java
+++ software/caintegrator2-war/src/gov/nih/nci/caintegrator2/application/workspace/WorkspaceServiceImpl.java
@@ -165,9 +165,6 @@ public class WorkspaceServiceImpl implements WorkspaceService {
         for (StudySubscription subscription : workspace.getSubscriptionCollection()) {
             if (subscription.getStudy().equals(study)) {
                 workspace.getSubscriptionCollection().remove(subscription);
-                if (workspace.getDefaultSubscription().equals(subscription)) {
-                    workspace.setDefaultSubscription(null);
-                }
                 saveUserWorkspace(workspace);
                 dao.delete(subscription);
                 return;
