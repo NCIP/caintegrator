f8578d66066e25f224109713efbadee586898622
Timothy Andrews <tandrews@scenpro.com>
2009-01-23 18:14:55 +0000

    Made searching for data elements asynchronous and updates the search page on the fly using DWR + Reverse Ajax.
    Gforge Tracker #18735
    Peer Reviewed by Eric

--- software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/web/ajax/DataElementSearchAjaxUpdaterTest.java
+++ software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/web/ajax/DataElementSearchAjaxUpdaterTest.java
@@ -85,14 +85,16 @@
  */
 package gov.nih.nci.caintegrator2.web.ajax;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import gov.nih.nci.cadsr.freestylesearch.util.SearchException;
 import gov.nih.nci.caintegrator2.AcegiAuthenticationStub;
-import gov.nih.nci.caintegrator2.external.ncia.NCIADicomJob;
-import gov.nih.nci.caintegrator2.external.ncia.NCIAFacadeStub;
-import gov.nih.nci.caintegrator2.web.SessionHelper;
+import gov.nih.nci.caintegrator2.application.study.StudyManagementServiceStub;
+import gov.nih.nci.caintegrator2.domain.annotation.AnnotationDefinition;
+import gov.nih.nci.caintegrator2.domain.annotation.CommonDataElement;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.acegisecurity.context.SecurityContextHolder;
 import org.directwebremoting.WebContextFactory;
@@ -102,40 +104,111 @@ import org.junit.Test;
 import com.opensymphony.xwork2.ActionContext;
 
 
-public class DicomRetrievalAjaxUpdaterTest {
+public class DataElementSearchAjaxUpdaterTest {
 
-    private DicomRetrievalAjaxUpdater updater;
-    private NCIAFacadeStub nciaFacade;
-    
+    private DataElementSearchAjaxUpdater updater;
+    private StudyManagementServiceStubForSearch studyManagementService;
+
     @Before
     public void setUp() throws Exception {
-        updater = new DicomRetrievalAjaxUpdater();
-        nciaFacade = new NCIAFacadeStub();
-        updater.setNciaFacade(nciaFacade);
+        updater = new DataElementSearchAjaxUpdater();
+        studyManagementService = new StudyManagementServiceStubForSearch();
+        studyManagementService.clear();
+        updater.setStudyManagementService(studyManagementService);
         SecurityContextHolder.getContext().setAuthentication(new AcegiAuthenticationStub());
         ActionContext.getContext().setSession(new HashMap<String, Object>());
-        NCIADicomJob dicomJob = new NCIADicomJob();
-        dicomJob.setCompleted(false);
-        dicomJob.setCurrentlyRunning(true);
-        dicomJob.getImageSeriesIDs().add("123");
-        dicomJob.getImageStudyIDs().add("345");
-        SessionHelper.getInstance().getDisplayableUserWorkspace().setDicomJob(dicomJob);
         WebContextFactory.setWebContextBuilder(new WebContextBuilderStub());
     }
 
     @Test
-    public void testRunDicomJob() throws InterruptedException {
-        updater.runDicomJob();
-        // Testing asynchronously requires sleep for a second.
-        while (updater.getDicomJob().isCurrentlyRunning()) {
-            Thread.sleep(1000);
+    public void testRunSearch() throws InterruptedException {
+        String entityType = "subject";
+        String studyConfId = "1";
+        String fileColId = "1";
+        String keywords = "test keywords";
+        updater.runSearch(entityType, studyConfId, fileColId, keywords);
+        updater.runSearch(entityType, studyConfId, fileColId, keywords);
+        while (true) {
+            Thread.sleep(600);
+            if (!updater.isCurrentlyRunning()) { // Need to verify all Threads are not running before proceeding.
+                break;
+            }
         }
-        assertTrue(nciaFacade.retrieveDicomFilesCalled);
-        assertFalse(updater.getDicomJob().isCurrentlyRunning());
-        nciaFacade.clear();
-        SessionHelper.getInstance().getDisplayableUserWorkspace().setDicomJob(null);
-        updater.runDicomJob();
-        assertFalse(nciaFacade.retrieveDicomFilesCalled);
+        assertTrue(studyManagementService.getMatchingDataElementsCalled);
+        assertTrue(studyManagementService.getMatchingDefinitionsCalled);
+        
+        keywords = "exceptionTest";
+        entityType = "image";
+        updater.runSearch(entityType, studyConfId, fileColId, keywords);
+        while (true) {
+            Thread.sleep(600);
+            if (!updater.isCurrentlyRunning()) { // Need to verify all Threads are not running before proceeding.
+                break;
+            }
+        }
+        
+        entityType = "image";
+        keywords = "null";
+        updater.runSearch(entityType, studyConfId, fileColId, keywords);
+        while (true) {
+            Thread.sleep(600);
+            if (!updater.isCurrentlyRunning()) { // Need to verify all Threads are not running before proceeding.
+                break;
+            }
+        }
+        keywords = null;
+        updater.runSearch(entityType, studyConfId, fileColId, keywords);
+    }
+
+    @Test
+    public void testInitializeJsp() throws InterruptedException {
+        updater.initializeJsp();
+        String entityType = "subject";
+        String studyConfId = "1";
+        String fileColId = "1";
+        String keywords = "test keywords";
+        updater.runSearch(entityType, studyConfId, fileColId, keywords);
+        Thread.sleep(600);
+        assertTrue(updater.isCurrentlyRunning());
+        updater.initializeJsp();
+        assertTrue(updater.isCurrentlyRunning());
+    }
+    
+    private class StudyManagementServiceStubForSearch extends StudyManagementServiceStub {        
+        @Override
+        public List<CommonDataElement> getMatchingDataElements(List<String> keywords) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            if ("exceptionTest".equals(keywords.get(0))) {
+                throw new SearchException("Failure Test!");
+            }
+            if ("null".equals(keywords.get(0))) {
+                return null;
+            }
+            super.getMatchingDataElements(keywords);
+            List<CommonDataElement> cdes = new ArrayList<CommonDataElement>();
+            cdes.add(new CommonDataElement());
+            cdes.add(new CommonDataElement());
+            return cdes;
+        }
+        
+        @Override
+        public List<AnnotationDefinition> getMatchingDefinitions(List<String> keywords) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            if ("null".equals(keywords.get(0))) {
+                return null;
+            }
+            super.getMatchingDefinitions(keywords);
+            List<AnnotationDefinition> annotationDefinitions = new ArrayList<AnnotationDefinition>();
+            annotationDefinitions.add(new AnnotationDefinition());
+            annotationDefinitions.add(new AnnotationDefinition());
+            return annotationDefinitions;
+        }
     }
 
 }
