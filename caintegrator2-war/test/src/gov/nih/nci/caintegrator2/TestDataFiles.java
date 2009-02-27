c02a020fbfa3d70b3a6818e4f411d97e71c06e57
J.P. Marple <jpmarple@5amsolutions.com>
2009-02-27 15:59:50 +0000

    User Story: none. Peer Review:  TJ. Andrews.
    Removal of the non-public DC Lung test.  Also, for the public DC Lung study, I used the new caarray experiment recently created on STAGE, and added valid control samples for that experiment.

--- caintegrator2-war/test/src/gov/nih/nci/caintegrator2/TestDataFiles.java
+++ caintegrator2-war/test/src/gov/nih/nci/caintegrator2/TestDataFiles.java
@@ -143,10 +143,10 @@ public class TestDataFiles {
     public static final File DC_LUNG_SAMPLE_MAPPING_FULL_FILE = getFile(DC_LUNG_SAMPLE_MAPPING_FULL_FILE_PATH);
     public static final File DC_LUNG_ANNOTATION_DEFINITIONS_FILE = getFile(DC_LUNG_ANNOTATION_DEFINITIONS_FILE_PATH);
     
-    // Files for public DC Lung Study (liu-00133 in array-qa-vm.nci.nih.gov)
+    // Files for public DC Lung Study (liu-00216 in array-stage.nci.nih.gov)
     public static final String DC_LUNG_PUBLIC_CLINICAL_FILE_PATH = "/dc_lung_clinical_data_public.csv";
-    public static final String DC_LUNG_PUBLIC_SAMPLE_MAPPING_FILE_PATH = "/DCLungStudy_full_array_mapping_load_qa.csv";
-    public static final String DC_LUNG_PUBLIC_CONTROL_SAMPLE_MAPPING_FILE_PATH = "/DCLungStudy_full_control_samples_load.csv";
+    public static final String DC_LUNG_PUBLIC_SAMPLE_MAPPING_FILE_PATH = "/dc_lung_sample_mapping.csv";
+    public static final String DC_LUNG_PUBLIC_CONTROL_SAMPLE_MAPPING_FILE_PATH = "/dc_lung_control_samples_moff.csv";
     public static final String DC_LUNG_PUBLIC_ANNOTATION_DEFINITIONS_FILE_PATH = "/dc_lung_public_annotation_definitions.csv";
 
     public static final File DC_LUNG_PUBLIC_CLINICAL_FILE = getFile(DC_LUNG_PUBLIC_CLINICAL_FILE_PATH);
