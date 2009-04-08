81adb833f228e25e44c4d13c95bb84ffc0cac4d6
J.P. Marple <jpmarple@5amsolutions.com>
2009-04-08 14:47:47 +0000

    User Story:  [#20175] Help - update to context sensitive online help. Peer Review:  N. Nguyen
    This partial commit updates the files produced by the help publishing system.

--- caintegrator2-war/web/help/wwhdata/js/search/pairs/pair44.js
+++ caintegrator2-war/web/help/wwhdata/js/search/pairs/pair44.js
@@ -1,16 +1,15 @@
 function FileData_Pairs(x)
 {
-x.t("asdfl","lasdlfk");
 x.t("kaplan-meier","plots");
+x.t("gene","expression");
 x.t("studies","performing");
 x.t("creating","kaplan-meier");
-x.t("queries","asdfl");
-x.t("queries","analyzing");
-x.t("queries","plot");
 x.t("analyzing","studies");
 x.t("performing","data");
+x.t("expression","analyzing");
+x.t("expression","plot");
 x.t("data","analysis");
 x.t("plots","plot");
-x.t("plot","queries");
+x.t("plot","gene");
 x.t("analysis","creating");
 }
