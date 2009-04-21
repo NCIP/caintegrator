06781803ba9c52f31359d3f54ccb4e0c623c6178
Eric Tavela <eric.tavela2@nih.gov>
2009-04-21 17:22:19 +0000

    User Story 19675: Implemented SegmentData and associated classes and database mappings and table. Peer reviewed by JP.

--- caintegrator2-war/src/gov/nih/nci/caintegrator2/domain/genomic/SegmentData.java
+++ caintegrator2-war/src/gov/nih/nci/caintegrator2/domain/genomic/SegmentData.java
@@ -83,116 +83,89 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package gov.nih.nci.caintegrator2.application.study;
+package gov.nih.nci.caintegrator2.domain.genomic;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import gov.nih.nci.caintegrator2.domain.AbstractCaIntegrator2Object;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
-
 /**
- * Base class for test data generators. These provide support for creating test objects and comparing two test objects. These were
- * designed principally to test persistence, but may be applicable whenever test data is necessary. 
- * 
- * <p>Subclasses must provide implementations of:
- * 
- * <ul>
- *  <li><code>createPersistentObject</code>: create an instance of the class handled by the generator.</li>
- *  <li><code>setValues</code>: set a unique or changed set of values on all fields within the instance.</li>
- *  <li><code>compareFields</code>: compare all the fields between two instances asserting that they are equal..</li>
- * </ul>
+ * Represents data for a single chromosomal segment copy number variation.
  */
-public abstract class AbstractTestDataGenerator<T> {
+public class SegmentData extends AbstractCaIntegrator2Object implements Comparable<SegmentData> {
+
+    private static final long serialVersionUID = 1L;
     
-    private static int uniqueInt;
-    
-    public final void compare(T original, T retrieved) {
-        if (original == null) {
-            assertNull(retrieved);
+    private Integer numberOfMarkers;
+    private Float segmentValue;
+    private ChromosomalLocation location;
+    private ArrayData arrayData;
+
+    /**
+     * {@inheritDoc}
+     */
+    public int compareTo(SegmentData segmentData) {
+        if (getLocation() == null) {
+            return segmentData.getLocation() == null ? 0 : 1;
+        } else if (segmentData.getLocation() == null) {
+            return -1;
         } else {
-            assertEquals(original, retrieved);
-        }
-        if (getId(original) != null) {
-            assertEquals(getId(original), getId(retrieved));
-        }
-        compareFields(original, retrieved);
-    }
-
-    public Long getId(T object) {
-        try {
-            return (Long) object.getClass().getMethod("getId").invoke(object);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Object doesn't have a getId method", e);
+            return location.compareTo(segmentData.getLocation());
         }
     }
 
-    public abstract void compareFields(T original, T retrieved);
-
-    public abstract void setValues(T object, Set<AbstractCaIntegrator2Object> nonCascadedObjects);
-
-    public abstract T createPersistentObject();
-
-    public T createPopulatedPersistentObject(Set <AbstractCaIntegrator2Object> nonCascadedObjects) {
-        T object = createPersistentObject();
-        setValues(object, nonCascadedObjects);
-        return object;
+    /**
+     * @return the numberOfMarkers
+     */
+    public Integer getNumberOfMarkers() {
+        return numberOfMarkers;
     }
 
-    protected String getUniqueString() {
-        return String.valueOf(getUniqueInt());
+    /**
+     * @param numberOfMarkers the numberOfMarkers to set
+     */
+    public void setNumberOfMarkers(Integer numberOfMarkers) {
+        this.numberOfMarkers = numberOfMarkers;
     }
 
-    protected String getUniqueString(int maxLength) {
-        String value = getUniqueString();
-        if (value.length() > maxLength) {
-            return value.substring(value.length() - maxLength);
-        } else {
-            return value;
-        }
+    /**
+     * @return the segmentValue
+     */
+    public Float getSegmentValue() {
+        return segmentValue;
     }
 
-    protected int getUniqueInt() {
-        return uniqueInt++;
-    }
-    
-    protected float getUniqueFloat() {
-        return (float) getUniqueInt();
-    }
-
-    protected Character getUniqueChar() {
-        return (char) (getUniqueInt() % 255);
+    /**
+     * @param segmentValue the segmentValue to set
+     */
+    public void setSegmentValue(Float segmentValue) {
+        this.segmentValue = segmentValue;
     }
 
-    @SuppressWarnings("hiding")
-    protected <T extends Enum<?>> T getNewEnumValue(T enumValue, T[] values) {
-        if (enumValue == null || enumValue.ordinal() == (values.length - 1)) {
-            return values[0];
-        } else {
-            return values[enumValue.ordinal() + 1];
-        }
+    /**
+     * @return the location
+     */
+    public ChromosomalLocation getLocation() {
+        return location;
     }
 
-    @SuppressWarnings("hiding")
-    protected <T> void compareCollections(Collection<T> originalCollection, Collection<T> retrievedCollection, AbstractTestDataGenerator<T> generator) {
-        assertEquals(originalCollection.size(), retrievedCollection.size());
-        for (Iterator<T> retrievedIterator = retrievedCollection.iterator(); retrievedIterator.hasNext();) {
-            T retrieved = retrievedIterator.next();
-            T original = getOriginal(originalCollection, retrieved);
-            generator.compare(original, retrieved);
-        }
+    /**
+     * @param location the location to set
+     */
+    public void setLocation(ChromosomalLocation location) {
+        this.location = location;
     }
 
-    @SuppressWarnings("hiding")
-    private <T> T getOriginal(Collection<T> originalCollection, T retrieved) {
-        for (T original : originalCollection) {
-            if (original.equals(retrieved)) {
-                return original;
-            }
-        }
-        return null;
+    /**
+     * @return the arrayData
+     */
+    public ArrayData getArrayData() {
+        return arrayData;
+    }
+
+    /**
+     * @param arrayData the arrayData to set
+     */
+    public void setArrayData(ArrayData arrayData) {
+        this.arrayData = arrayData;
     }
 
 }
