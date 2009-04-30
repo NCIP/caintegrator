64e0589412ce10c6d224f36c5a9c5a50f7eb278f
Nghiem Nguyen <nguyenn@mail.nih.gov>
2009-04-30 14:14:57 +0000

    Item 20865 – Populate the Configuration_Properties table, reviewed by JP.

---
+++ common/resources/db-upgrade/mysql/db-init-application-properties.sql
@@ -0,0 +1,5 @@
+truncate table configuration_parameter;
+insert into configuration_parameter (parameter, raw_value) values
+    ("GRID_INDEX_URL", "@grid.index.url@"),
+    ("PREPROCESS_DATASET_URL", "http://node255.broad.mit.edu:6060/wsrf/services/cagrid/PreprocessDatasetMAGEService"),
+    ("COMPARATIVE_MARKER_SELECTION_URL", "http://node255.broad.mit.edu:6060/wsrf/services/cagrid/ComparativeMarkerSelMAGESvc");
