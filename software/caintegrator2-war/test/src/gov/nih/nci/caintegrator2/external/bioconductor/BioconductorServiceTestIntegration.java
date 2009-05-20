25cc6cc2fcfde7da066c16e99baa7974ee92ba28
Eric Tavela <eric.tavela2@nih.gov>
2009-05-20 16:32:18 +0000

    User Story 19676: Revised integration tests to pass with new copy number platform classes. Peer reviewed by Ngoc.

--- software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/external/bioconductor/BioconductorServiceTestIntegration.java
+++ software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/external/bioconductor/BioconductorServiceTestIntegration.java
@@ -2,7 +2,7 @@ package gov.nih.nci.caintegrator2.external.bioconductor;
 
 import gov.nih.nci.caintegrator2.TestArrayDesignFiles;
 import gov.nih.nci.caintegrator2.TestDataFiles;
-import gov.nih.nci.caintegrator2.application.arraydata.AffymetrixExpressionPlatformSource;
+import gov.nih.nci.caintegrator2.application.arraydata.AffymetrixDnaPlatformSource;
 import gov.nih.nci.caintegrator2.application.arraydata.ArrayDataService;
 import gov.nih.nci.caintegrator2.application.arraydata.ArrayDataValueType;
 import gov.nih.nci.caintegrator2.application.arraydata.ArrayDataValues;
@@ -20,6 +20,7 @@ import gov.nih.nci.caintegrator2.external.DataRetrievalException;
 import gov.nih.nci.caintegrator2.external.ServerConnectionProfile;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -79,7 +80,9 @@ public class BioconductorServiceTestIntegration extends AbstractTransactionalSpr
     private Platform getOrCreatePlatform() throws PlatformLoadingException {
         Platform platform = dao.getPlatform("Mapping50K_Hind240");
         if (platform == null) {
-            AffymetrixExpressionPlatformSource source = new AffymetrixExpressionPlatformSource(TestArrayDesignFiles.MAPPING_50K_HIND_ANNOTATION_FILE);
+            List<File> files = new ArrayList<File>();
+            files.add(TestArrayDesignFiles.MAPPING_50K_HIND_ANNOTATION_FILE);
+            AffymetrixDnaPlatformSource source = new AffymetrixDnaPlatformSource(files, "Mapping50K_Hind240");
             platform = getArrayDataService().loadArrayDesign(source);
         }
         return platform;
