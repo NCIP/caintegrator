0cbd08780eef519eb75b10a8240cd048a3c3eecb
J.P. Marple <jpmarple@5amsolutions.com>
2009-06-16 12:00:04 +0000

    User story: [#21339] Gene Symbols should contain a link to CGAP site. Reviewed by: Ngoc Nguyen.
    A client side check that the input parameter to CGAP is not empty or does not contain only spaces.

--- caintegrator2-war/web/common/js/caintegrator2.js
+++ caintegrator2-war/web/common/js/caintegrator2.js
@@ -19,5 +19,22 @@ function checkanddisplay(chk){
 
 function gotoCGAP(cgapUrl, geneSymbols) {
     var url = cgapUrl + document.getElementById(geneSymbols).value;
+    
+    var holdString = trimIt(document.getElementById(geneSymbols).value);
+    
+    if(holdString=="") {
+        alert("No values entered.  Please enter a gene symbol.");
+        return false;
+    }
+    
     window.open(url, 'cai2_CGAP');
 }
+
+function trimIt (str) {
+    var str = str.replace(/^\s\s*/, ''),
+        ws = /\s/,
+        i = str.length;
+    while (ws.test(str.charAt(--i)));
+    return str.slice(0, i + 1);
+}
+
