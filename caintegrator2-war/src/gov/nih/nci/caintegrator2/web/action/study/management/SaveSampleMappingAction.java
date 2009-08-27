af00893b294060d33f040dab87a0b4f9d590ae6c
Timothy Andrews <tandrews@scenpro.com>
2009-08-27 19:44:00 +0000

    Combined Control Sample Mapping and Sample Mapping into one action and allow the form to upload one or both of these files with one button.
    GForge Tracker #22940
    Peer reviewed by Eric.

--- caintegrator2-war/src/gov/nih/nci/caintegrator2/web/action/study/management/SaveSampleMappingAction.java
+++ caintegrator2-war/src/gov/nih/nci/caintegrator2/web/action/study/management/SaveSampleMappingAction.java
@@ -90,6 +90,8 @@ import gov.nih.nci.caintegrator2.application.study.ValidationException;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.lang.StringUtils;
+
 
 /**
  * Action called to create or edit a <code>GenomicDataSourceConfiguration</code>.
@@ -98,9 +100,19 @@ public class SaveSampleMappingAction extends AbstractGenomicSourceAction {
 
     private static final long serialVersionUID = 1L;
     
+    /////////
+    // Sample Mapping
+    /////////
     private File sampleMappingFile;
     private String sampleMappingFileContentType;
     private String sampleMappingFileFileName;
+    /////////
+    // Control Mapping
+    /////////
+    private String controlSampleSetName;
+    private File controlSampleFile;
+    private String controlSampleFileContentType;
+    private String controlSampleFileFileName;
 
     /**
      * {@inheritDoc}
@@ -115,21 +127,52 @@ public class SaveSampleMappingAction extends AbstractGenomicSourceAction {
      */
     @Override
     public String execute() {
-        try {
-            getStudyManagementService().mapSamples(getStudyConfiguration(), getSampleMappingFile());
-            persistFileName();
-            return SUCCESS;
-        } catch (ValidationException e) {
-            setFieldError("Invalid file: " + e.getResult().getInvalidMessage());
-            return INPUT;
-        } catch (IOException e) {
-            setFieldError("Unexpected IO exception - " + e.getMessage());
-            return INPUT;
-        } catch (Exception e) {
-            setFieldError("Something is very wrong in the code, please report this problem - " + e.getMessage());
-            return INPUT;
-        } 
+        String returnString = SUCCESS;
+        returnString = executeSampleMapping();
+        if (SUCCESS.equals(returnString)) {
+            returnString = executeControlSampleMapping();
+        }
+        return returnString;
         
+    }
+
+    private String executeControlSampleMapping() {
+        if (getControlSampleFile() != null) {
+            try {
+                getStudyManagementService().addControlSampleSet(getGenomicSource(),
+                        getControlSampleSetName(), getControlSampleFile(), getControlSampleFileFileName());
+                return SUCCESS;
+            } catch (ValidationException e) {
+                setControlMappingFieldError("Invalid file: " + e.getResult().getInvalidMessage());
+                return INPUT;
+            } catch (IOException e) {
+                setControlMappingFieldError("Unexpected IO exception: " + e.getMessage());
+                return INPUT;
+            } 
+        }
+        return SUCCESS;
+    }
+
+
+    private String executeSampleMapping() {
+        if (getSampleMappingFile() != null) {
+            try {
+                getStudyManagementService().mapSamples(getStudyConfiguration(), getSampleMappingFile());
+                persistFileName();
+                return SUCCESS;
+            } catch (ValidationException e) {
+                setSampleMappingFieldError("Invalid file: " + e.getResult().getInvalidMessage());
+                return INPUT;
+            } catch (IOException e) {
+                setSampleMappingFieldError("Unexpected IO exception - " + e.getMessage());
+                return INPUT;
+            } catch (Exception e) {
+                setSampleMappingFieldError("Something is very wrong in the code, please report this problem - " 
+                        + e.getMessage());
+                return INPUT;
+            }
+        }
+        return SUCCESS;
     }
 
     private void persistFileName() {
@@ -137,9 +180,12 @@ public class SaveSampleMappingAction extends AbstractGenomicSourceAction {
         getStudyManagementService().save(getStudyConfiguration());
     }
     
-    private void setFieldError(String errorMessage) {
+    private void setSampleMappingFieldError(String errorMessage) {
         addFieldError("sampleMappingFile", errorMessage);
-        
+    }
+    
+    private void setControlMappingFieldError(String errorMessage) {
+        addFieldError("sampleMappingFile", errorMessage);
     }
     
     /**
@@ -147,12 +193,33 @@ public class SaveSampleMappingAction extends AbstractGenomicSourceAction {
      */
     @Override
     public void validate() {
-        if (sampleMappingFile == null) {
-            addFieldError("sampleMappingFile", " File is required");
-        } else if (sampleMappingFile.length() == 0) {
-            addFieldError("sampleMappingFile", " File is empty");
+        if (sampleMappingFile == null && controlSampleFile == null) {
+            addActionError("File is required.");
+        } 
+        if (sampleMappingFile != null) {
+            validateSampleMappingFile();
+        }
+        if (controlSampleFile != null) {
+            validateControlMappingFile();
         }
         prepareValueStack();
+    }
+
+    private void validateControlMappingFile() {
+        if (StringUtils.isEmpty(getControlSampleSetName())) {
+            addFieldError("controlSampleSetName", "Name is required");
+        } else if (getStudyConfiguration().getControlSampleSet(getControlSampleSetName()) != null) {
+            addFieldError("controlSampleSetName", "Duplicate name, please enter a new name.");
+        }
+        if (getControlSampleFile().length() == 0) {
+            setControlMappingFieldError("File is empty");
+        }
+    }
+
+    private void validateSampleMappingFile() {
+        if (sampleMappingFile.length() == 0) {
+            setSampleMappingFieldError("File is empty");
+        }
     }
 
 
@@ -196,6 +263,62 @@ public class SaveSampleMappingAction extends AbstractGenomicSourceAction {
      */
     public void setSampleMappingFileFileName(String sampleMappingFileFileName) {
         this.sampleMappingFileFileName = sampleMappingFileFileName;
+    }
+
+    /**
+     * @return the controlSampleSetName
+     */
+    public String getControlSampleSetName() {
+        return controlSampleSetName;
+    }
+
+    /**
+     * @param controlSampleSetName the controlSampleSetName to set
+     */
+    public void setControlSampleSetName(String controlSampleSetName) {
+        this.controlSampleSetName = controlSampleSetName;
+    }
+
+    /**
+     * @return the controlSampleFile
+     */
+    public File getControlSampleFile() {
+        return controlSampleFile;
+    }
+
+    /**
+     * @param controlSampleFile the controlSampleFile to set
+     */
+    public void setControlSampleFile(File controlSampleFile) {
+        this.controlSampleFile = controlSampleFile;
+    }
+
+    /**
+     * @return the controlSampleFileContentType
+     */
+    public String getControlSampleFileContentType() {
+        return controlSampleFileContentType;
+    }
+
+    /**
+     * @param controlSampleFileContentType the controlSampleFileContentType to set
+     */
+    public void setControlSampleFileContentType(String controlSampleFileContentType) {
+        this.controlSampleFileContentType = controlSampleFileContentType;
+    }
+
+    /**
+     * @return the controlSampleFileFileName
+     */
+    public String getControlSampleFileFileName() {
+        return controlSampleFileFileName;
+    }
+
+    /**
+     * @param controlSampleFileFileName the controlSampleFileFileName to set
+     */
+    public void setControlSampleFileFileName(String controlSampleFileFileName) {
+        this.controlSampleFileFileName = controlSampleFileFileName;
     }
        
 }
