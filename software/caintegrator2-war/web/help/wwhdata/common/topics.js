9feba34f8acfea6b7bfa61708c2b381ec0bfafc0
J.P. Marple <jpmarple@5amsolutions.com>
2009-09-02 22:03:21 +0000

    User Story: None.  Updating the online help. Peer review: TJ Andrews.
    Latest update from the online help publisher.

--- software/caintegrator2-war/web/help/wwhdata/common/topics.js
+++ software/caintegrator2-war/web/help/wwhdata/common/topics.js
@@ -1,30 +1,30 @@
 function  WWHBookData_MatchTopic(P)
 {
 var C=null;
-if(P=="login_help")C="Getting_Started.2.4.html#1115560";
-if(P=="welcome_help")C="Getting_Started.2.5.html#1127723";
-if(P=="logging_out")C="Getting_Started.2.7.html#1128246";
+if(P=="login_help")C="Getting_Started.2.3.html#1115560";
+if(P=="welcome_help")C="Getting_Started.2.4.html#1127723";
+if(P=="logging_out")C="Getting_Started.2.6.html#1128246";
 if(P=="edit_study_help")C="CreatingNewStudy.3.4.html#1073921";
 if(P=="define_fields_help")C="CreatingNewStudy.3.5.html#1075197";
 if(P=="assign_annotations_help")C="CreatingNewStudy.3.6.html#1082187";
 if(P=="search_annotation_definitions_help")C="CreatingNewStudy.3.7.html#1082197";
-if(P=="survival_values_help")C="CreatingNewStudy.3.9.html#1077698";
-if(P=="edit_genomic_data_help")C="CreatingNewStudy.3.10.html#1076975";
-if(P=="edit_sample_mappings_help")C="CreatingNewStudy.3.12.html#1081247";
-if(P=="edit_imaging_data_help")C="CreatingNewStudy.3.14.html#1075831";
-if(P=="manage_study_help")C="CreatingNewStudy.3.16.html#1076280";
-if(P=="manage_platforms_help")C="CreatingNewStudy.3.17.html#1078043";
+if(P=="survival_values_help")C="CreatingNewStudy.3.8.html#1077698";
+if(P=="edit_genomic_data_help")C="CreatingNewStudy.3.9.html#1076975";
+if(P=="edit_sample_mappings_help")C="CreatingNewStudy.3.10.html#1081247";
+if(P=="edit_imaging_data_help")C="CreatingNewStudy.3.13.html#1075831";
+if(P=="manage_study_help")C="CreatingNewStudy.3.15.html#1076280";
+if(P=="manage_platforms_help")C="CreatingNewStudy.3.16.html#1078043";
 if(P=="search_criteria_help")C="SearchingWithinAStudy.4.3.html#1074305";
 if(P=="search_columns_help")C="SearchingWithinAStudy.4.4.html#1074610";
 if(P=="save_query_help")C="SearchingWithinAStudy.4.6.html#1075754";
-if(P=="search_results_help")C="View%20Results.5.2.html#1167812";
-if(P=="DICOM_retrieval_help")C="View%20Results.5.5.html#1164183";
+if(P=="query_results_help")C="View%20Search%20Results.5.2.html#1167812";
+if(P=="DICOM_retrieval_help")C="View%20Search%20Results.5.7.html#1171422";
 if(P=="KM_annotation_help")C="PerformingAnalysis.6.4.html#1073490";
-if(P=="KM_gene-expression_help")C="PerformingAnalysis.6.5.html#1073810";
+if(P=="KM_gene-expression_help")C="PerformingAnalysis.6.5.html#1080552";
 if(P=="KM_queries_help")C="PerformingAnalysis.6.6.html#1073776";
-if(P=="GE_plot_annotation_help")C="PerformingAnalysis.6.7.html#1073419";
-if(P=="GE_plot_gen_queries_help")C="PerformingAnalysis.6.8.html#1077369";
-if(P=="GE_plot_clin_queries_help")C="PerformingAnalysis.6.9.html#1077378";
-if(P=="gene_pattern_help")C="PerformingAnalysis.6.10.html#1077363";
+if(P=="GE_plot_annotation_help")C="PerformingAnalysis.6.8.html#1084201";
+if(P=="GE_plot_gen_queries_help")C="PerformingAnalysis.6.9.html#1083368";
+if(P=="GE_plot_clin_queries_help")C="PerformingAnalysis.6.10.html#1077378";
+if(P=="gene_pattern_help")C="PerformingAnalysis.6.12.html#1077363";
 return C;
 }
