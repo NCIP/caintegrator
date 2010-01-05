a5553f6bf4b441c7b1103de7b6a8b86e0dd978ed
Timothy Andrews <tandrews@scenpro.com>
2010-01-05 16:31:21 +0000

    Updated the model for SubjectList / SubjectListCriterion to be able to save lists of subjects as well as query them later.
    GForge Tracker #21757
    Peer reviewed by Ngoc

---
+++ caintegrator2-war/src/gov/nih/nci/caintegrator2/domain/application/SubjectListCriterion.java
@@ -0,0 +1,48 @@
+package gov.nih.nci.caintegrator2.domain.application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ */
+public class SubjectListCriterion extends AbstractCriterion implements Cloneable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private Set<SubjectList> subjectListCollection = new HashSet<SubjectList>();
+
+    
+    /**
+     * @return the subjectListCollection
+     */
+    public Set<SubjectList> getSubjectListCollection() {
+        return subjectListCollection;
+    }
+
+    /**
+     * @param subjectListCollection the subjectListCollection to set
+     */
+    public void setSubjectListCollection(Set<SubjectList> subjectListCollection) {
+        this.subjectListCollection = subjectListCollection;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected SubjectListCriterion clone() throws CloneNotSupportedException {
+        SubjectListCriterion clone = (SubjectListCriterion) super.clone();
+        clone.getSubjectListCollection().addAll(cloneValueCollection());
+        return clone;
+    }
+    
+    private Set<SubjectList> cloneValueCollection() {
+        Set<SubjectList> clone = new HashSet<SubjectList>();
+        for (SubjectList subjectList : subjectListCollection) {
+            clone.add(subjectList);
+        }
+        return clone;
+    }
+
+}
