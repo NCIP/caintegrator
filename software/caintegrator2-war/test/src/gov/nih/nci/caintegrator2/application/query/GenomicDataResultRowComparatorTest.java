a49fa5fde47fface56c3bc168512c2d364eb8c5a
Nghiem Nguyen <nguyenn@mail.nih.gov>
2010-07-28 19:53:24 +0000

    Sort the result rows with segment data, reviewed by TJ.

--- software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/application/query/GenomicDataResultRowComparatorTest.java
+++ software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/application/query/GenomicDataResultRowComparatorTest.java
@@ -87,6 +87,9 @@ package gov.nih.nci.caintegrator2.application.query;
 
 import static org.junit.Assert.assertEquals;
 import gov.nih.nci.caintegrator2.domain.application.GenomicDataResultRow;
+import gov.nih.nci.caintegrator2.domain.application.GenomicDataResultValue;
+import gov.nih.nci.caintegrator2.domain.application.SegmentDataResultValue;
+import gov.nih.nci.caintegrator2.domain.genomic.ChromosomalLocation;
 import gov.nih.nci.caintegrator2.domain.genomic.Gene;
 import gov.nih.nci.caintegrator2.domain.genomic.GeneExpressionReporter;
 
@@ -136,6 +139,90 @@ public class GenomicDataResultRowComparatorTest {
         assertEquals(resultRow2, resultRows.get(1));
         assertEquals(resultRow1, resultRows.get(2));
         
+        // Test segment data result
+        resultRow1 = new GenomicDataResultRow();
+        SegmentDataResultValue  segmentDataResultValue1 = new SegmentDataResultValue();
+        segmentDataResultValue1.setChromosomalLocation(createChromosomalLocation("1", 111, 777));
+        resultRow1.setSegmentDataResultValue(segmentDataResultValue1);
+        resultRow2 = new GenomicDataResultRow();
+        SegmentDataResultValue  segmentDataResultValue2 = new SegmentDataResultValue();
+        segmentDataResultValue2.setChromosomalLocation(createChromosomalLocation("1", 111, 666));
+        resultRow2.setSegmentDataResultValue(segmentDataResultValue2);
+        resultRow3 = new GenomicDataResultRow();
+        SegmentDataResultValue  segmentDataResultValue3 = new SegmentDataResultValue();
+        segmentDataResultValue3.setChromosomalLocation(createChromosomalLocation("1", 111, 555));
+        resultRow3.setSegmentDataResultValue(segmentDataResultValue3);
+        
+        resultRows.clear();
+        resultRows.add(resultRow1);
+        resultRows.add(resultRow2);
+        resultRows.add(resultRow3);
+
+        Collections.sort(resultRows, new GenomicDataResultRowComparator());
+
+        assertEquals(resultRow3, resultRows.get(0));
+        assertEquals(resultRow2, resultRows.get(1));
+        assertEquals(resultRow1, resultRows.get(2));
+        
+        // Test segment data result
+        resultRow1 = new GenomicDataResultRow();
+         segmentDataResultValue1 = new SegmentDataResultValue();
+        segmentDataResultValue1.setChromosomalLocation(createChromosomalLocation("1", 111, 777));
+        resultRow1.setSegmentDataResultValue(segmentDataResultValue1);
+        resultRow2 = new GenomicDataResultRow();
+         segmentDataResultValue2 = new SegmentDataResultValue();
+        segmentDataResultValue2.setChromosomalLocation(createChromosomalLocation("1", 333, 666));
+        resultRow2.setSegmentDataResultValue(segmentDataResultValue2);
+        resultRow3 = new GenomicDataResultRow();
+         segmentDataResultValue3 = new SegmentDataResultValue();
+        segmentDataResultValue3.setChromosomalLocation(createChromosomalLocation("1", 222, 555));
+        resultRow3.setSegmentDataResultValue(segmentDataResultValue3);
+        
+        resultRows.clear();
+        resultRows.add(resultRow1);
+        resultRows.add(resultRow2);
+        resultRows.add(resultRow3);
+
+        Collections.sort(resultRows, new GenomicDataResultRowComparator());
+
+        assertEquals(resultRow1, resultRows.get(0));
+        assertEquals(resultRow3, resultRows.get(1));
+        assertEquals(resultRow2, resultRows.get(2));
+        
+        // Test segment data result
+        resultRow1 = new GenomicDataResultRow();
+         segmentDataResultValue1 = new SegmentDataResultValue();
+        segmentDataResultValue1.setChromosomalLocation(createChromosomalLocation("2", 111, 777));
+        resultRow1.setSegmentDataResultValue(segmentDataResultValue1);
+        resultRow2 = new GenomicDataResultRow();
+         segmentDataResultValue2 = new SegmentDataResultValue();
+        segmentDataResultValue2.setChromosomalLocation(createChromosomalLocation("3", 111, 666));
+        resultRow2.setSegmentDataResultValue(segmentDataResultValue2);
+        resultRow3 = new GenomicDataResultRow();
+         segmentDataResultValue3 = new SegmentDataResultValue();
+        segmentDataResultValue3.setChromosomalLocation(createChromosomalLocation("1", 111, 555));
+        resultRow3.setSegmentDataResultValue(segmentDataResultValue3);
+        
+        resultRows.clear();
+        resultRows.add(resultRow1);
+        resultRows.add(resultRow2);
+        resultRows.add(resultRow3);
+
+        Collections.sort(resultRows, new GenomicDataResultRowComparator());
+
+        assertEquals(resultRow3, resultRows.get(0));
+        assertEquals(resultRow1, resultRows.get(1));
+        assertEquals(resultRow2, resultRows.get(2));
+        
+        
+    }
+    
+    private ChromosomalLocation createChromosomalLocation(String chromosome, int start, int end) {
+        ChromosomalLocation chromosomalLocation = new ChromosomalLocation();
+        chromosomalLocation.setChromosome(chromosome);
+        chromosomalLocation.setStartPosition(start);
+        chromosomalLocation.setEndPosition(end);
+        return chromosomalLocation;
     }
 
 }
