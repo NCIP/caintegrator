b7279673c94309baa680b48a577a9ace85a945b7
Timothy Andrews <tandrews@scenpro.com>
2010-08-16 15:02:01 +0000

    Fix fold change calculation to work with 2 color agilent.
    No Gforge issue.
    Peer reviewed by JP.

--- caintegrator2-war/test/src/gov/nih/nci/caintegrator2/application/arraydata/ArrayDataServiceTest.java
+++ caintegrator2-war/test/src/gov/nih/nci/caintegrator2/application/arraydata/ArrayDataServiceTest.java
@@ -195,10 +195,10 @@ public class ArrayDataServiceTest {
         ArrayDataValues foldChangeValues = service.getFoldChangeValues(request, controlValuesList, null);
         assertEquals(2, foldChangeValues.getArrayDatas().size());
         assertEquals(2, foldChangeValues.getReporters().size());
-        assertEquals(1.752, (float) foldChangeValues.getFloatValue(data1, reporter1, ArrayDataValueType.EXPRESSION_SIGNAL), 0.0001);
-        assertEquals(5.256, (float) foldChangeValues.getFloatValue(data2, reporter1, ArrayDataValueType.EXPRESSION_SIGNAL), 0.0001);
-        assertEquals(7.2886, (float) foldChangeValues.getFloatValue(data1, reporter2, ArrayDataValueType.EXPRESSION_SIGNAL), 0.0001);
-        assertEquals(14.5772, (float) foldChangeValues.getFloatValue(data2, reporter2, ArrayDataValueType.EXPRESSION_SIGNAL), 0.0001);
+        assertEquals(1.3905, (float) foldChangeValues.getFloatValue(data1, reporter1, ArrayDataValueType.EXPRESSION_SIGNAL), 0.0001);
+        assertEquals(4.1716, (float) foldChangeValues.getFloatValue(data2, reporter1, ArrayDataValueType.EXPRESSION_SIGNAL), 0.0001);
+        assertEquals(5.7849, (float) foldChangeValues.getFloatValue(data1, reporter2, ArrayDataValueType.EXPRESSION_SIGNAL), 0.0001);
+        assertEquals(11.5699, (float) foldChangeValues.getFloatValue(data2, reporter2, ArrayDataValueType.EXPRESSION_SIGNAL), 0.0001);
         
         // Test getFoldChangeValues by query
         Query query = new Query();
