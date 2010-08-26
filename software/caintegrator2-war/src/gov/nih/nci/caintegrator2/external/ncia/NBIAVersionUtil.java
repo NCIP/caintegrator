e900d8b93b10deb4fe747f9dae0812e0cecb14e8
Timothy Andrews <tandrews@scenpro.com>
2010-08-26 17:51:37 +0000

    Allow for caI2 to work against NBIA 4.4 or below OR 4.5 by checking the attributes on the “Series” object to see if the given NBIA URL links to a 4.4 or 4.5 instance, and queries it appropriately.
    GForge Tracker #29460
    Peer reviewed by Ngoc

--- software/caintegrator2-war/src/gov/nih/nci/caintegrator2/external/ncia/NBIAVersionUtil.java
+++ software/caintegrator2-war/src/gov/nih/nci/caintegrator2/external/ncia/NBIAVersionUtil.java
@@ -85,32 +85,54 @@
  */
 package gov.nih.nci.caintegrator2.external.ncia;
 
+import gov.nih.nci.cagrid.metadata.MetadataUtils;
+import gov.nih.nci.cagrid.metadata.common.UMLAttribute;
+import gov.nih.nci.cagrid.metadata.common.UMLClassUmlAttributeCollection;
+import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
+import gov.nih.nci.cagrid.metadata.dataservice.DomainModelExposedUMLClassCollection;
+import gov.nih.nci.cagrid.metadata.dataservice.UMLClass;
 import gov.nih.nci.cagrid.metadata.exceptions.ResourcePropertyRetrievalException;
-import gov.nih.nci.caintegrator2.external.ConnectionException;
-import gov.nih.nci.caintegrator2.external.ServerConnectionProfile;
 
-import java.rmi.RemoteException;
-
+import org.apache.axis.message.addressing.EndpointReferenceType;
+import org.apache.axis.types.URI;
 import org.apache.axis.types.URI.MalformedURIException;
 
+
 /**
- * Implementation of the NCIAServiceFactory.
+ * Utility class that looks at the "Series" object to determine if NBIA is version 4.4 or 4.5.
  */
-public class NCIAServiceFactoryImpl implements NCIAServiceFactory {
+public final class NBIAVersionUtil {
+
+    private NBIAVersionUtil() { }
 
     /**
-     * {@inheritDoc}
+     * 
+     * @param nbiaUrl connection URL.
+     * @return version of NBIA.
+     * @throws MalformedURIException - if unable to connect to NBIA.
+     * @throws ResourcePropertyRetrievalException - if unable to connect to NBIA.
      */
-    public NCIASearchService createNCIASearchService(ServerConnectionProfile profile) throws ConnectionException {
-        try {
-            return new NCIASearchServiceImpl(profile, NBIAVersionUtil.getVersion(profile.getUrl()));
-        } catch (MalformedURIException e) {
-            throw new ConnectionException("Malformed URI.", e);
-        } catch (RemoteException e) {
-            throw new ConnectionException("Remote Connection Failed.", e);
-        } catch (ResourcePropertyRetrievalException e) {
-            throw new ConnectionException("Remote Connection Failed while trying to look up NBIA version.", e);
+    public static NBIAVersionEnum getVersion(String nbiaUrl) throws MalformedURIException, 
+        ResourcePropertyRetrievalException  {
+        EndpointReferenceType endpoint = new EndpointReferenceType(new URI(nbiaUrl));
+        DomainModel domainModel = MetadataUtils.getDomainModel(endpoint);
+        DomainModelExposedUMLClassCollection classes = domainModel.getExposedUMLClassCollection();
+        for (UMLClass umlClass : classes.getUMLClass()) {
+            if (umlClass.getClassName().equals("Series")) {
+                return isNbiaV44Series(umlClass) ? NBIAVersionEnum.V4_4 : NBIAVersionEnum.V4_5;
+            }
         }
+        throw new IllegalStateException("Never came across Series UML Class if we got here");
+    }
+
+    private static boolean isNbiaV44Series(UMLClass seriesClass) {
+        UMLClassUmlAttributeCollection attrCollection = seriesClass.getUmlAttributeCollection();
+        for (UMLAttribute umlClassAttribute : attrCollection.getUMLAttribute()) {
+            if (NBIAVersionEnum.V4_4.getSeriesIdAtt().equals(umlClassAttribute.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
