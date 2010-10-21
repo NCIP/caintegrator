962a62f96d25ed74ab01cea5d7902979a8882a70
J.P. Marple <jpmarple@5amsolutions.com>
2010-10-21 22:04:52 +0000

    User Story: None.  Adding logging to bioconductor calls to compare the logs on the bioconductor server. Reviewed by: pending.

--- caintegrator2-war/src/gov/nih/nci/caintegrator2/external/bioconductor/BioconductorServiceImpl.java
+++ caintegrator2-war/src/gov/nih/nci/caintegrator2/external/bioconductor/BioconductorServiceImpl.java
@@ -131,7 +131,9 @@ public class BioconductorServiceImpl implements BioconductorService {
             parameter.setEarlyStoppingCriterion(configuration.getEarlyStoppingCriterion());
             parameter.setPermutationReplicates(configuration.getPermutationReplicates());
             parameter.setRandomNumberSeed(configuration.getRandomNumberSeed());
+            LOGGER.info("Begin Retrieving segment from BioConductor");
             DerivedDNAcopySegment segment = client.getDerivedDNAcopySegment(assays, parameter);
+            LOGGER.info("End Retrieving segment from BioConductor");
             addSegmentationData(segment, dnaAnalysisData);
         } catch (RemoteException e) {
             LOGGER.error("Couldn't complete CaDNACopy job", e);
