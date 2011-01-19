1d5a14fe841e4e379aea506ac9a30a2408f06408
Nghiem Nguyen <nguyenn@mail.nih.gov>
2011-01-19 20:31:39 +0000

    Show CGHCalls on the query results page, reviewed by TJ.

--- caintegrator2-war/src/gov/nih/nci/caintegrator2/domain/application/GenomicDataResultValue.java
+++ caintegrator2-war/src/gov/nih/nci/caintegrator2/domain/application/GenomicDataResultValue.java
@@ -12,9 +12,85 @@ public class GenomicDataResultValue extends AbstractCaIntegrator2Object {
     private static final long serialVersionUID = 1L;
     private static final String HIGH_VARIANCE_APPEND_STRING = "**";
     private Float value;
+    private Integer callsValue;
+    private Float probabilityAmplification;
+    private Float probabilityGain;
+    private Float probabilityLoss;
+    private Float probabilityNormal;
+
     private GenomicDataResultColumn column;
     private GenomicCriteriaMatchTypeEnum criteriaMatchType = GenomicCriteriaMatchTypeEnum.NO_MATCH;
     private boolean highVariance = false;
+    
+    /**
+     * @return the callsValue
+     */
+    public Integer getCallsValue() {
+        return callsValue;
+    }
+
+    /**
+     * @param callsValue the callsValue to set
+     */
+    public void setCallsValue(Integer callsValue) {
+        this.callsValue = callsValue;
+    }
+
+    /**
+     * @return the probabilityAmplification
+     */
+    public Float getProbabilityAmplification() {
+        return probabilityAmplification;
+    }
+
+    /**
+     * @param probabilityAmplification the probabilityAmplification to set
+     */
+    public void setProbabilityAmplification(Float probabilityAmplification) {
+        this.probabilityAmplification = probabilityAmplification;
+    }
+
+    /**
+     * @return the probabilityGain
+     */
+    public Float getProbabilityGain() {
+        return probabilityGain;
+    }
+
+    /**
+     * @param probabilityGain the probabilityGain to set
+     */
+    public void setProbabilityGain(Float probabilityGain) {
+        this.probabilityGain = probabilityGain;
+    }
+
+    /**
+     * @return the probabilityLoss
+     */
+    public Float getProbabilityLoss() {
+        return probabilityLoss;
+    }
+
+    /**
+     * @param probabilityLoss the probabilityLoss to set
+     */
+    public void setProbabilityLoss(Float probabilityLoss) {
+        this.probabilityLoss = probabilityLoss;
+    }
+
+    /**
+     * @return the probabilityNormal
+     */
+    public Float getProbabilityNormal() {
+        return probabilityNormal;
+    }
+
+    /**
+     * @param probabilityNormal the probabilityNormal to set
+     */
+    public void setProbabilityNormal(Float probabilityNormal) {
+        this.probabilityNormal = probabilityNormal;
+    }
     
     /**
      * 
@@ -58,7 +134,6 @@ public class GenomicDataResultValue extends AbstractCaIntegrator2Object {
     public boolean isMeetsCriterion() {
         return !GenomicCriteriaMatchTypeEnum.NO_MATCH.equals(criteriaMatchType);
     }
-
     
     /**
      * For the JSP to get the color to highlight based on the value.
@@ -70,6 +145,22 @@ public class GenomicDataResultValue extends AbstractCaIntegrator2Object {
                                     : GenomicCriteriaMatchTypeEnum.UNDER.getHighlightColor();    
         }
         return criteriaMatchType.getHighlightColor();
+    }
+
+    /**
+     * @return color the calls value
+     */
+    public boolean isColorCalls() {
+        return callsValue != 0;
+    }
+    
+    /**
+     * For the JSP to get the color to highlight based on the calls value.
+     * @return highlight color for the Calls value.
+     */
+    public String getHighlightColorCalls() {
+        return callsValue > 0 ? GenomicCriteriaMatchTypeEnum.OVER.getHighlightColor() 
+                                    : GenomicCriteriaMatchTypeEnum.UNDER.getHighlightColor();   
     }
 
     /**
