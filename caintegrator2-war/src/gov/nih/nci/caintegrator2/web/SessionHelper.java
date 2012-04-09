5231665899d07376abe92f13c1cff20226059db4
Abraham Evans-EL <evanselaj@mail.nih.gov>
2012-04-09 13:20:44 +0000

    CAINT-1097: Fixing stack trace when adding too long of a description to a Gene or Subject list.

--- caintegrator2-war/src/gov/nih/nci/caintegrator2/web/SessionHelper.java
+++ caintegrator2-war/src/gov/nih/nci/caintegrator2/web/SessionHelper.java
@@ -1,86 +1,86 @@
 /**
  * The software subject to this notice and license includes both human readable
  * source code form and machine readable, binary, object code form. The caIntegrator2
- * Software was developed in conjunction with the National Cancer Institute 
+ * Software was developed in conjunction with the National Cancer Institute
  * (NCI) by NCI employees, 5AM Solutions, Inc. (5AM), ScenPro, Inc. (ScenPro)
- * and Science Applications International Corporation (SAIC). To the extent 
- * government employees are authors, any rights in such works shall be subject 
- * to Title 17 of the United States Code, section 105. 
+ * and Science Applications International Corporation (SAIC). To the extent
+ * government employees are authors, any rights in such works shall be subject
+ * to Title 17 of the United States Code, section 105.
  *
- * This caIntegrator2 Software License (the License) is between NCI and You. You (or 
- * Your) shall mean a person or an entity, and all other entities that control, 
- * are controlled by, or are under common control with the entity. Control for 
- * purposes of this definition means (i) the direct or indirect power to cause 
+ * This caIntegrator2 Software License (the License) is between NCI and You. You (or
+ * Your) shall mean a person or an entity, and all other entities that control,
+ * are controlled by, or are under common control with the entity. Control for
+ * purposes of this definition means (i) the direct or indirect power to cause
  * the direction or management of such entity, whether by contract or otherwise,
- * or (ii) ownership of fifty percent (50%) or more of the outstanding shares, 
- * or (iii) beneficial ownership of such entity. 
+ * or (ii) ownership of fifty percent (50%) or more of the outstanding shares,
+ * or (iii) beneficial ownership of such entity.
  *
- * This License is granted provided that You agree to the conditions described 
- * below. NCI grants You a non-exclusive, worldwide, perpetual, fully-paid-up, 
- * no-charge, irrevocable, transferable and royalty-free right and license in 
- * its rights in the caIntegrator2 Software to (i) use, install, access, operate, 
+ * This License is granted provided that You agree to the conditions described
+ * below. NCI grants You a non-exclusive, worldwide, perpetual, fully-paid-up,
+ * no-charge, irrevocable, transferable and royalty-free right and license in
+ * its rights in the caIntegrator2 Software to (i) use, install, access, operate,
  * execute, copy, modify, translate, market, publicly display, publicly perform,
- * and prepare derivative works of the caIntegrator2 Software; (ii) distribute and 
- * have distributed to and by third parties the caIntegrator2 Software and any 
- * modifications and derivative works thereof; and (iii) sublicense the 
- * foregoing rights set out in (i) and (ii) to third parties, including the 
- * right to license such rights to further third parties. For sake of clarity, 
- * and not by way of limitation, NCI shall have no right of accounting or right 
- * of payment from You or Your sub-licensees for the rights granted under this 
+ * and prepare derivative works of the caIntegrator2 Software; (ii) distribute and
+ * have distributed to and by third parties the caIntegrator2 Software and any
+ * modifications and derivative works thereof; and (iii) sublicense the
+ * foregoing rights set out in (i) and (ii) to third parties, including the
+ * right to license such rights to further third parties. For sake of clarity,
+ * and not by way of limitation, NCI shall have no right of accounting or right
+ * of payment from You or Your sub-licensees for the rights granted under this
  * License. This License is granted at no charge to You.
  *
- * Your redistributions of the source code for the Software must retain the 
- * above copyright notice, this list of conditions and the disclaimer and 
- * limitation of liability of Article 6, below. Your redistributions in object 
- * code form must reproduce the above copyright notice, this list of conditions 
- * and the disclaimer of Article 6 in the documentation and/or other materials 
- * provided with the distribution, if any. 
+ * Your redistributions of the source code for the Software must retain the
+ * above copyright notice, this list of conditions and the disclaimer and
+ * limitation of liability of Article 6, below. Your redistributions in object
+ * code form must reproduce the above copyright notice, this list of conditions
+ * and the disclaimer of Article 6 in the documentation and/or other materials
+ * provided with the distribution, if any.
  *
- * Your end-user documentation included with the redistribution, if any, must 
- * include the following acknowledgment: This product includes software 
- * developed by 5AM, ScenPro, SAIC and the National Cancer Institute. If You do 
- * not include such end-user documentation, You shall include this acknowledgment 
- * in the Software itself, wherever such third-party acknowledgments normally 
+ * Your end-user documentation included with the redistribution, if any, must
+ * include the following acknowledgment: This product includes software
+ * developed by 5AM, ScenPro, SAIC and the National Cancer Institute. If You do
+ * not include such end-user documentation, You shall include this acknowledgment
+ * in the Software itself, wherever such third-party acknowledgments normally
  * appear.
  *
  * You may not use the names "The National Cancer Institute", "NCI", "ScenPro",
- * "SAIC" or "5AM" to endorse or promote products derived from this Software. 
- * This License does not authorize You to use any trademarks, service marks, 
- * trade names, logos or product names of either NCI, ScenPro, SAID or 5AM, 
- * except as required to comply with the terms of this License. 
+ * "SAIC" or "5AM" to endorse or promote products derived from this Software.
+ * This License does not authorize You to use any trademarks, service marks,
+ * trade names, logos or product names of either NCI, ScenPro, SAID or 5AM,
+ * except as required to comply with the terms of this License.
  *
- * For sake of clarity, and not by way of limitation, You may incorporate this 
- * Software into Your proprietary programs and into any third party proprietary 
- * programs. However, if You incorporate the Software into third party 
+ * For sake of clarity, and not by way of limitation, You may incorporate this
+ * Software into Your proprietary programs and into any third party proprietary
+ * programs. However, if You incorporate the Software into third party
  * proprietary programs, You agree that You are solely responsible for obtaining
- * any permission from such third parties required to incorporate the Software 
+ * any permission from such third parties required to incorporate the Software
  * into such third party proprietary programs and for informing Your a
- * sub-licensees, including without limitation Your end-users, of their 
- * obligation to secure any required permissions from such third parties before 
- * incorporating the Software into such third party proprietary software 
- * programs. In the event that You fail to obtain such permissions, You agree 
- * to indemnify NCI for any claims against NCI by such third parties, except to 
- * the extent prohibited by law, resulting from Your failure to obtain such 
- * permissions. 
+ * sub-licensees, including without limitation Your end-users, of their
+ * obligation to secure any required permissions from such third parties before
+ * incorporating the Software into such third party proprietary software
+ * programs. In the event that You fail to obtain such permissions, You agree
+ * to indemnify NCI for any claims against NCI by such third parties, except to
+ * the extent prohibited by law, resulting from Your failure to obtain such
+ * permissions.
  *
- * For sake of clarity, and not by way of limitation, You may add Your own 
- * copyright statement to Your modifications and to the derivative works, and 
- * You may provide additional or different license terms and conditions in Your 
- * sublicenses of modifications of the Software, or any derivative works of the 
+ * For sake of clarity, and not by way of limitation, You may add Your own
+ * copyright statement to Your modifications and to the derivative works, and
+ * You may provide additional or different license terms and conditions in Your
+ * sublicenses of modifications of the Software, or any derivative works of the
  * Software as a whole, provided Your use, reproduction, and distribution of the
  * Work otherwise complies with the conditions stated in this License.
  *
- * THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES, 
- * (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, 
- * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO 
+ * THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO
  * EVENT SHALL THE NATIONAL CANCER INSTITUTE, 5AM SOLUTIONS, INC., SCENPRO, INC.,
- * SCIENCE APPLICATIONS INTERNATIONAL CORPORATION OR THEIR 
- * AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * SCIENCE APPLICATIONS INTERNATIONAL CORPORATION OR THEIR
+ * AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package gov.nih.nci.caintegrator2.web;
@@ -95,7 +95,6 @@ import gov.nih.nci.caintegrator2.domain.application.UserWorkspace;
 import gov.nih.nci.caintegrator2.security.SecurityHelper;
 import gov.nih.nci.caintegrator2.web.action.analysis.KMPlotMapper;
 import gov.nih.nci.caintegrator2.web.action.analysis.geneexpression.GEPlotMapper;
-import gov.nih.nci.logging.api.util.StringUtils;
 
 import java.io.File;
 import java.util.Map;
@@ -103,6 +102,7 @@ import java.util.Map;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionContext;
@@ -116,7 +116,7 @@ public final class SessionHelper {
      * .war file context name.
      */
     public static final String WAR_CONTEXT_NAME = "caintegrator";
-    private static final String SESSION_HELPER_SESSION_KEY = "sessionHelper"; 
+    private static final String SESSION_HELPER_SESSION_KEY = "sessionHelper";
     private static final String DISPLAYABLE_USER_WORKSPACE_SESSION_KEY = "displayableWorkspace";
     private static final String DISPLAYABLE_USER_WORKSPACE_VALUE_STACK_KEY = "displayableWorkspace";
     private static final String KM_PLOT_SESSION_KEY = "kmPlot";
@@ -127,17 +127,17 @@ public final class SessionHelper {
     private Boolean studyManager = null;
     private Boolean platformManager = null;
     private Boolean invalidDataBeingAccessed = false;
-    
+
     private SessionHelper() {
-        
+
     }
-    
+
     /**
      * Singleton method to get the instance off of the sessionMap object or create a new one.
      * @return - CSMUserHelper object.
      */
     public static SessionHelper getInstance() {
-        SessionHelper instance = 
+        SessionHelper instance =
             (SessionHelper) getSession().get(SESSION_HELPER_SESSION_KEY);
         if (instance == null) {
             instance = new SessionHelper();
@@ -145,7 +145,7 @@ public final class SessionHelper {
         }
         return instance;
     }
-    
+
     /**
      * Refreshes the objects held by this object so that that are up to date and attached
      * to the current Hibernate session. After calling this method, clients can expect the
@@ -157,10 +157,10 @@ public final class SessionHelper {
      *  <li><b>study</b> - The current <code>Study</code> if one has been selected</li>
      *  <li><b>query</b> - The current <code>Query</code> if one has been selected or is being defined</li>
      *  <li><b>queryResult</b> - The current <code>QueryResult</code> from the last clinical query executed</li>
-     *  <li><b>genomicDataQueryResult</b> - The current <code>GenomicDataQueryResult</code> from the last 
+     *  <li><b>genomicDataQueryResult</b> - The current <code>GenomicDataQueryResult</code> from the last
      *  genomic data query executed</li>
      * </ul>
-     * 
+     *
      * @param workspaceService session used to retrieve workspace.
      * @param isStudyNeedRefresh determines if we need to refresh study on the stack or not.
      */
@@ -177,9 +177,9 @@ public final class SessionHelper {
     public static String getIgvSessionUrl() {
         return getAnalysisViewerUrl(AnalysisViewerTypeEnum.IGV.getValue());
     }
-    
+
     /**
-     * 
+     *
      * @return the heatmap session url.
      */
     public static String getHeatmapSessionUrl() {
@@ -191,28 +191,28 @@ public final class SessionHelper {
         HttpServletRequest request = ServletActionContext.getRequest();
         return getRequestBaseUrl(request)
             + "/" + WAR_CONTEXT_NAME +  "/viewer/retrieveFile.jnlp?" + AnalysisViewerFileServlet.SESSION_PARAMETER + "="
-            + request.getRequestedSessionId() + "&" + AnalysisViewerFileServlet.VIEWERTYPE_PARAMETER + "=" 
+            + request.getRequestedSessionId() + "&" + AnalysisViewerFileServlet.VIEWERTYPE_PARAMETER + "="
             + analysisViewerType + "&" + AnalysisViewerFileServlet.FILENAME_PARAMETER + "=";
     }
-    
+
     /**
-     * 
+     *
      * @param context servlet context.
      * @return heatmap large bins file path.
      */
     public static String getHeatmapLargeBinsFile(ServletContext context) {
         return context.getRealPath(WEBINF_CLASSES_DIR + HeatmapFileTypeEnum.LARGE_BINS_FILE.getFilename());
     }
-    
+
     /**
-     * 
+     *
      * @param context servlet context.
      * @return heatmap small bins file path.
      */
     public static String getHeatmapSmallBinsFile(ServletContext context) {
         return context.getRealPath(WEBINF_CLASSES_DIR + HeatmapFileTypeEnum.SMALL_BINS_FILE.getFilename());
     }
-    
+
     /**
      * @return URL for the "/catingrator2/common" directory.
      */
@@ -239,9 +239,9 @@ public final class SessionHelper {
     public boolean isAuthenticated() {
         return getUsername() != null;
     }
-    
+
     /**
-     * 
+     *
      * @return if user is anonymously logged in.
      */
     public static boolean isAnonymousUser() {
@@ -273,27 +273,27 @@ public final class SessionHelper {
      * @return the struts token or empty string if no token is found.
      */
     public String getToken() {
-        
+
         String returnString = "";
-        
+
         if (ActionContext.getContext().getSession()
                 .get(org.apache.struts2.util.TokenHelper.DEFAULT_TOKEN_NAME) != null) {
             returnString =
                 (String) ActionContext.getContext().
                 getSession().get(org.apache.struts2.util.TokenHelper.DEFAULT_TOKEN_NAME);
         }
-        
+
         return returnString;
     }
-    
+
     /**
      * @return the struts token name
      */
     public String getTokenName() {
-        return (String) org.apache.struts2.util.TokenHelper.DEFAULT_TOKEN_NAME;
+        return org.apache.struts2.util.TokenHelper.DEFAULT_TOKEN_NAME;
 
-    }    
-        
+    }
+
     /**
      * @return the studyManager
      */
@@ -308,7 +308,7 @@ public final class SessionHelper {
     }
 
     /**
-     * 
+     *
      * @param studyManager the studyManager to set.
      */
     public void setStudyManager(boolean studyManager) {
@@ -334,24 +334,24 @@ public final class SessionHelper {
     public void setPlatformManager(Boolean platformManager) {
         this.platformManager = platformManager;
     }
-    
+
     /**
-     * 
+     *
      *  @param anonymousUserWorkspace anonymous user workspace to put on the session.
      */
     public static void setAnonymousUserWorkspace(UserWorkspace anonymousUserWorkspace) {
         getSession().put(ANONYMOUS_USER_WORKSPACE_SESSION_KEY, anonymousUserWorkspace);
     }
-    
+
     /**
-     * 
+     *
      * @return anonymous user workspace on the session.
      */
     public static UserWorkspace getAnonymousUserWorkspace() {
         return (UserWorkspace) getSession().get(ANONYMOUS_USER_WORKSPACE_SESSION_KEY);
     }
-    
-    
+
+
     /**
      * Uses a KMPlotMapper object to store multiple KMPlots on the value stack, based on
      * one per plot type.
@@ -365,9 +365,9 @@ public final class SessionHelper {
         KMPlotMapper map = (KMPlotMapper) getSession().get(KM_PLOT_SESSION_KEY);
         map.getKmPlotMap().put(plotType, kmPlot);
     }
-    
+
     /**
-     * 
+     *
      * @return the kmPlot on the ValueStack.
      */
     public static KMPlot getAnnotationBasedKmPlot() {
@@ -377,9 +377,9 @@ public final class SessionHelper {
         }
         return null;
     }
-    
+
     /**
-     * 
+     *
      * @return the kmPlot on the ValueStack.
      */
     public static KMPlot getGeneExpressionBasedKmPlot() {
@@ -389,9 +389,9 @@ public final class SessionHelper {
         }
         return null;
     }
-    
+
     /**
-     * 
+     *
      * @return the kmPlot on the ValueStack.
      */
     public static KMPlot getQueryBasedKmPlot() {
@@ -401,7 +401,7 @@ public final class SessionHelper {
         }
         return null;
     }
-    
+
     /**
      * Clears all plots off the session.
      */
@@ -411,7 +411,7 @@ public final class SessionHelper {
             mapper.clear();
         }
     }
-    
+
     /**
      * Uses a GEPlotMapper object to store multiple GEPlots on the value stack, based on
      * one per plot type.
@@ -427,7 +427,7 @@ public final class SessionHelper {
     }
 
     /**
-     * 
+     *
      * @return the Annotation gePlots on the ValueStack.
      */
     public static GeneExpressionPlotGroup getAnnotationBasedGePlots() {
@@ -437,9 +437,9 @@ public final class SessionHelper {
         }
         return null;
     }
-    
+
     /**
-     * 
+     *
      * @return the Genomic Query gePlots on the ValueStack.
      */
     public static GeneExpressionPlotGroup getGenomicQueryBasedGePlots() {
@@ -449,9 +449,9 @@ public final class SessionHelper {
         }
         return null;
     }
-    
+
     /**
-     * 
+     *
      * @return the Clinical Query gePlots on the ValueStack.
      */
     public static GeneExpressionPlotGroup getClinicalQueryBasedGePlots() {
@@ -471,7 +471,7 @@ public final class SessionHelper {
             mapper.clear();
         }
     }
-    
+
     /**
      * Determines if the currently requested page is authorized for user.
      * @return T/F value.
@@ -483,7 +483,7 @@ public final class SessionHelper {
         }
         return isAuthorizedPage;
     }
-    
+
     /**
      * Sets the flag for the requested page to be either T/F.
      * @param isAuthorized if the user is authorized or not to see page.
