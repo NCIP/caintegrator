c5cad3407da20e528df60a03ca13d3646d5eebcd
Abraham Evans-EL <aevansel@5amsolutions.com>
2013-01-24 21:36:38 -0500

    Updating licensing information and headers.

--- software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/security/SessionFactoryStub.java
+++ software/caintegrator2-war/test/src/gov/nih/nci/caintegrator2/security/SessionFactoryStub.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright 5AM Solutions Inc, ESAC, ScenPro & SAIC
+ *
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See https://github.com/NCIP/caintegrator/blob/master/LICENSE for details.
+ */
 package gov.nih.nci.caintegrator2.security;
 
 import java.io.Serializable;
