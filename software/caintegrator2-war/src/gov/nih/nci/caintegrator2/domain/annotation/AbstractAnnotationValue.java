ed136e944937131dc769699f50a4138e8abd59a6
Abraham Evans-EL <aevansel@5amsolutions.com>
2013-01-24 21:30:44 -0500

    Updating licensing information and headers.

--- software/caintegrator2-war/src/gov/nih/nci/caintegrator2/domain/annotation/AbstractAnnotationValue.java
+++ software/caintegrator2-war/src/gov/nih/nci/caintegrator2/domain/annotation/AbstractAnnotationValue.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright 5AM Solutions Inc, ESAC, ScenPro & SAIC
+ *
+ * Distributed under the OSI-approved BSD 3-Clause License.
+ * See https://github.com/NCIP/caintegrator/blob/master/LICENSE for details.
+ */
 package gov.nih.nci.caintegrator2.domain.annotation;
 
 import gov.nih.nci.caintegrator2.application.study.AnnotationTypeEnum;
