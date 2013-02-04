82cf74272918ceaf66a6029e42003034a3cceeb2
Abraham Evans-EL <aevansel@5amsolutions.com>
2013-02-04 14:37:36 -0500

    Updating license header to include new url for LICENSE.txt.

--- caintegrator-war/test/src/gov/nih/nci/caintegrator/application/query/domain/FoldChangeCriterionGenerator.java
+++ caintegrator-war/test/src/gov/nih/nci/caintegrator/application/query/domain/FoldChangeCriterionGenerator.java
@@ -2,7 +2,7 @@
  * Copyright 5AM Solutions Inc, ESAC, ScenPro & SAIC
  *
  * Distributed under the OSI-approved BSD 3-Clause License.
- * See https://github.com/NCIP/caintegrator/blob/master/LICENSE for details.
+ * See http://ncip.github.com/caintegrator/LICENSE.txt for details.
  */
 package gov.nih.nci.caintegrator.application.query.domain;
 
