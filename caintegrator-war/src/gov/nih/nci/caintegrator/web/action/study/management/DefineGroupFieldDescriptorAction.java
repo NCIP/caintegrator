ec2002dc6a32d5024ca975eb27a5acfd07ff56a8
Eric Tavela <etavela@5amsolutions.com>
2013-12-08 13:51:28 -0500

    CAINT-1350 Fixed incorrect bean scopes.

--- caintegrator-war/src/gov/nih/nci/caintegrator/web/action/study/management/DefineGroupFieldDescriptorAction.java
+++ caintegrator-war/src/gov/nih/nci/caintegrator/web/action/study/management/DefineGroupFieldDescriptorAction.java
@@ -9,7 +9,6 @@ package gov.nih.nci.caintegrator.web.action.study.management;
 import gov.nih.nci.caintegrator.application.study.AnnotationGroup;
 import gov.nih.nci.caintegrator.web.ajax.DataElementSearchAjaxUpdater;
 
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +16,7 @@ import org.springframework.stereotype.Component;
  * Action used to edit the type and annotation of a file column by a Study Manager.
  */
 @Component
-@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+@Scope("session")
 public class DefineGroupFieldDescriptorAction extends AbstractFieldDescriptorAction {
 
     private static final long serialVersionUID = 1L;
